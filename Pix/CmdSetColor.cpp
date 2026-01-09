f3baa1a436b4a4829020db329555db8e88f88d69
RovanDhaliwal <Rovandhaliwal101@gmail.com>
2026-01-08 21:59:15 -0800

    Second
    pushing second time

--- Pix/CmdSetColor.cpp
+++ Pix/CmdSetColor.cpp
@@ -7,9 +7,11 @@ bool CmdSetColor::Execute(const std::vector<std::string>& params)
 {
 	// Need at least 3 params for r, g, b
 	if (params.size() < 3)
+	{
 		return false;
+	}
 
-	auto vc = VariableCache::Get();
+	VariableCache* vc = VariableCache::Get();
 	float r = vc->GetFloat(params[0]);
 	float g = vc->GetFloat(params[1]);
 	float b = vc->GetFloat(params[2]);
